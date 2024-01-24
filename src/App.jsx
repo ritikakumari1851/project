eca9128b775a6754931ce181fb6e9d755be219d3
Ritika <ritikakri3708@gmail.com>
2024-01-24 11:43:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Routes, Route} from 'react-router-dom'
+function App() {
+ 
+
+  return (
+  <>
+  <div className=' text-blue-900 text-4xl bg-red-900'>
+  helo
+  </div>
+ </>
+  )
+}
+
+export default App
